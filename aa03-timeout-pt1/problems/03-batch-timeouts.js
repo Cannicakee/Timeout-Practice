7c4a7c5ce8f5242bc051af43dab7bb9cf9157390
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-04-15 23:40:35 -0400

    solved problem 3

--- aa03-timeout-pt1/problems/03-batch-timeouts.js
+++ aa03-timeout-pt1/problems/03-batch-timeouts.js
@@ -33,8 +33,22 @@ console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 ***********************************************************************/
 
 function batchTimeouts(callbacks, delays) {
-  // Your code here 
+  return callbacks.map((el, i) => {return setTimeout(el, delays[i])}) 
 }
+
+const sayHello = () => console.log('hi');
+const sayGoodbye = () => console.log('bye');
+const shout = () => console.log('WHAT?');
+const tasks = [sayHello, sayGoodbye, shout];
+const delays = [500, 200, 900];
+
+const timeoutObjs = batchTimeouts(tasks, delays); 
+// should print: 
+//  'bye' after 200 ms
+//  'hi' after 500 ms
+//  'WHAT?' after 900 ms
+
+console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
