5c5b4a14eb42ae98f43b74c9e11fcd2aea6e53a5
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-04-15 23:46:05 -0400

    solved problem 4

--- aa03-timeout-pt1/problems/04-interval-count.js
+++ aa03-timeout-pt1/problems/04-interval-count.js
@@ -17,8 +17,20 @@ intervalCount(function() {
 ***********************************************************************/
 
 function intervalCount(cb, delay, amount) {
-  // Your code here 
+  const intCount = setInterval(() => {
+    // console.log(cb)
+    cb()
+    amount--
+    
+    if (amount === 0) { 
+      clearInterval(intCount)
+    }
+  }, delay)
 }
+
+intervalCount(function() {
+  console.log('hi');
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
