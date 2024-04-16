8a42351fafb8efcf5af7ffd1d1951c244ce7b516
Cannicakee <stanfill11@gmail.com>
2024-04-15 21:26:44 -0600

    Solved Problem 1

--- aa03-timeout-pt1/problems/01-set-seconds-timeout.js
+++ aa03-timeout-pt1/problems/01-set-seconds-timeout.js
@@ -24,9 +24,16 @@ setSecondsTimeout(function () {
 ***********************************************************************/
 
 function setSecondsTimeout(cb, delayInSeconds) {
-  // Your code here 
+  return setTimeout(cb, delayInSeconds * 1000)
 }
 
+setSecondsTimeout(function () {
+  console.log('hello');
+}, 1); // should print 'hello' after 1000 milliseconds
+
+setSecondsTimeout(function () {
+  console.log('world');
+}, 1.4); // should print 'world' after 1400 milliseconds
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = setSecondsTimeout;
